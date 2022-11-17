b88480665b1079662bbadf9ceda1435f914c35d7
Wreford <wrefordmessi@gmail.com>
2022-11-17 04:08:49 -0800

    creating parent node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_node - creates a binary tree node func
+ * @parent: parent of the node to create
+ * @value: value to store in parent node
+ *
+ * Return: pointer to the new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	node->parent = parent;
+	node->n = value;
+	node->left = NULL;
+	node->left = NULL;
+	return (node);
+}
