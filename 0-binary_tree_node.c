617b9752e35345e1660f51a81cb7c76e3565a5de
Wreford <wrefordmessi@gmail.com>
2022-11-17 10:56:16 -0800

    node creation updates

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_node - creates a binary tree node func
+ * binary_tree_node - creates a binary tree node
  * @parent: parent of the node to create
- * @value: value to store in parent node
+ * @value: value to store in new node
  *
  * Return: pointer to the new node
  */
@@ -13,13 +13,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = malloc(sizeof(binary_tree_t));
+
 	if (node == NULL)
-	{
 		return (NULL);
-	}
+
 	node->parent = parent;
 	node->n = value;
 	node->left = NULL;
-	node->left = NULL;
+	node->right = NULL;
 	return (node);
 }
